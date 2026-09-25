126fb3ccd92ee49bf96a86306c694615073c3a26
estevaovicente-jpg <estevao.vicente@escola.pr.gov.br>
2026-09-25 13:42:31 +0000

    commit 1

---
+++ script.js
@@ -0,0 +1,22 @@
+function calcularMedia()
+const textoNota1 = document.getElementById("nota1").value;
+const textoNota2 = document.getElementById("nota2").value;
+const textoNota3 = document.getElementById("nota3").value;
+
+const nota1 = Number(textoNota1);
+const nota2 = Number(textoNota2);
+const nota3 = Number(textoNota3);
+
+const media = (nota1 + nota2 + nota3) / 3;
+let situacao;
+
+if (media >= 7) {
+    situacao = "Aprovado";
+} else if (media >= 5) {
+    situacao = "Recuperação";
+} else {
+    situacao = "Reprovado";
+}
+
+const mediaFormatada = media.toFixed(1);
+document.getElementById("resultado").innerHTML = `Média: ${mediaFormatada} - Situação: ${situacao}`;
